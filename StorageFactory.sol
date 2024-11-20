4fa35cfde76c1803c3098989148c0b56d5b00719
avcisec <yasaravci@protonmail.com>
2024-11-20 17:53:55 +0100

    ABI

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -8,18 +8,31 @@ pragma solidity  0.8.24;
 // import kullanirken her zaman sol dosyasindaki spesifik kontrati belirt. 
 // Cost Efficiency acisindan import {contrat ismi} from "kontratlar.sol"; seklinde kullan.
 
-import {SimpleStorage, SimpleStorage2} from "./Contracts/SimpleStorage.sol";
+import {SimpleStorage} from "./Contracts/SimpleStorage.sol";
 
-contract StorageFactory{
+contract StorageFactory {
 // uint256 public favoritenumber
 // type     visibility  name 
-    SimpleStorage public simpleStorage;
+     SimpleStorage[] public ListOfSimpleStorageContracts;
+   // address[] public ListOfSimpleStorageAddresses;
 // Bu satırda, sözleşme içinde simpleStorage adında bir durum değişkeni (state variable) tanımlanmış. 
 // Türü ise SimpleStorage. public belirteci sayesinde bu değişkene sözleşme dışından da erişilebilir.
 // Ancak burada simpleStorage henüz bir kontrata işaret etmiyor; sadece bu değişkenin hangi türde olduğu belirtiliyor.
 // Değişkene bir değer (örneğin bir kontrat adresi) atanmadan kullanılamaz.
+
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        ListOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // kontrat ile iletisime gecmek icin address ve ABI (Application Binary Interface) gerekir.
+       // SimpleStorage mySimpleStorage = ListOfSimpleStorageContracts[_simpleStorageIndex];
+        
+       ListOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+
     }
 
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
+        return ListOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
 }
