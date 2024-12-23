f228f35e10ee26198e5ba16a947a051b0a1f15f3
avcisec <yasaravci@protonmail.com>
2024-12-23 20:23:14 +0100

    chainlink price feed, library etc.

--- FundMe.sol
+++ FundMe.sol
@@ -1,17 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {priceConverter} from "./priceConverter.sol";
+
 contract FundMe {
+
+    using priceConverter for uint256;
+    
+    uint256 public minimumUsd = 50e18;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
     function fund() public payable  {
-// kullanicilarin para gonderebilmesine izin ver
-// minimum tutar belirle
-// Bir sayinin ussunu belirtirken ** kullaniriz.
 
+        // kullanicilarin para gonderebilmesine izin ver
+        // minimum tutar belirle
+        // Bir sayinin ussunu belirtirken ** kullaniriz.
 
-    require(msg.value > 1e18, "didn't send enough ETH");   // 1e18 = 1 ETH = = 1000000000000000000 = 1 * 10 ** 18
-
+     require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH");   // 1e18 = 1 ETH = = 1000000000000000000 = 1 * 10 ** 18
+    funders.push(msg.sender);
+    addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
- //   function withdraw() public {}
+        // function withdraw() public {}
 
+ 
 
 }
